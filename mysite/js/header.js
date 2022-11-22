8c949a4ea60548c37ee684e93e131851f0c5ea2f
Jared Nash <nash_jar000@byui.edu>
2022-11-22 15:54:31 -0800

    added header JS

---
+++ mysite/js/header.js
@@ -0,0 +1,26 @@
+header.innerHTML = `
+<header>
+            <a href="index.html">
+                <img class="logo" src="images/mylogo.png" alt="Stay Connected With Jared Logo">
+            </a>
+            <nav>
+                <a href="index.html">Home</a>
+                <a href="video-journals.html">Video Journals</a>
+                <a href="about-me.html">About Me (FAQ)</a>
+                <a href="piano.html">Piano Videos</a>
+                <!-- <a href="contact.html">Contact me</a> -->
+                <!-- <a href="coming-soon.html">Coming Soon</a> -->
+                <a href="mission.html">Mission Audio Files</a>
+                <a href="favorite-music.html">Favorite Music</a>
+                <a href="painting.html">Paintings</a>
+                <a href="recipies.html">Recipes</a>
+                <a href="videos.html">Other Videos</a>
+                <script>
+                    function darklightmode(){
+                        var element = document.body;
+                        element.classList.toggle("dark-mode");
+                    }
+                </script>
+            </nav>
+        </header>
+`;
