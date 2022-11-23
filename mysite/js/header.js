a3d6e0bfe43536f00d142b0bb849e55372fac120
Jared Nash <nash_jar000@byui.edu>
2022-11-22 16:06:05 -0800

    adjusted the heading a bit

--- mysite/js/header.js
+++ mysite/js/header.js
@@ -6,15 +6,15 @@ header.innerHTML = `
             <nav>
                 <a href="index.html">Home</a>
                 <a href="video-journals.html">Video Journals</a>
-                <a href="about-me.html">About Me (FAQ)</a>
                 <a href="piano.html">Piano Videos</a>
+                <a href="videos.html">Other Videos</a>
+                <a href="about-me.html">About Me (FAQ)</a>
                 <!-- <a href="contact.html">Contact me</a> -->
                 <!-- <a href="coming-soon.html">Coming Soon</a> -->
-                <a href="mission.html">Mission Audio Files</a>
+                <!-- <a href="mission.html">Mission Audio Files</a> -->
                 <a href="favorite-music.html">Favorite Music</a>
                 <a href="painting.html">Paintings</a>
                 <a href="recipies.html">Recipes</a>
-                <a href="videos.html">Other Videos</a>
                 <script>
                     function darklightmode(){
                         var element = document.body;
