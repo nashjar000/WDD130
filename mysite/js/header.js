d20cfaad354f3d1e310ff56a2cae3c3284eb2fde
Jared Nash <nash_jar000@byui.edu>
2022-12-31 22:43:36 -0700

    normal site for REAL this time, haha.

--- mysite/js/header.js
+++ mysite/js/header.js
@@ -1,7 +1,7 @@
 header.innerHTML = `
 <header>
- <!-- <link rel="stylesheet" href="styles/style.css"> -->
-<link rel="stylesheet" href="styles/Christmas-theme.css">  
+ <link rel="stylesheet" href="styles/style.css"> 
+<!-- <link rel="stylesheet" href="styles/Christmas-theme.css">  -->
  <!-- <link rel="stylesheet" href="styles/Halloween-theme.css"> -->
 
 <style type="text/css">
@@ -45,7 +45,7 @@ header.innerHTML = `
                 element.classList.toggle("dark-mode");
             } 
         </script> -->
-        <img class="snow-background" src="images/christmas-background-with-falling-snow_275806-1176.webp" alt="snow-background"> 
+        <!-- <img class="snow-background" src="images/christmas-background-with-falling-snow_275806-1176.webp" alt="snow-background"> -->
         
         <!-- <div class="storm"></div>
         <a hidden>
