484f29e4c6845a7ea7c55d5dd28ed18ad18580dc
Jared Nash <nash_jar000@byui.edu>
2023-01-25 08:44:41 -0700

    adjusted the IG logo on the footer

---
+++ mysite/js/footer.js
@@ -0,0 +1,14 @@
+footer.innerHTML = `
+<footer>
+        <h3>Stay Connected With Jared &copy; 2022 - Jared Nash</h3>
+        <h3><a href="site-plan.html">Site Plan</a></h3>
+        <div class="social">
+            <a href="https://www.facebook.com/jared.nash.108/" target="_blank">
+                <img src="images/facebook.png" alt="fb icon">
+            </a>
+
+            <a href="https://www.instagram.com/jared_nash0509/" target="_blank">
+                <img src="images/Instagram_logo.png" alt="instagram icon" width="64" height="64">
+            </a>
+</footer>
+`
