c42d58bd072d549824e0eb816ce8e9a51516d3a1
Jared Nash <nash_jar000@byui.edu>
2023-08-26 17:44:01 -0700

    added a scroll to top button

--- mysite/js/header.js
+++ mysite/js/header.js
@@ -3,6 +3,9 @@ header.innerHTML = `
  <link rel="stylesheet" href="styles/style.css"> 
  <!-- <link rel="stylesheet" href="styles/Christmas-theme.css">  -->
  <!-- <link rel="stylesheet" href="styles/Halloween-theme.css"> -->
+ <script defer src="./js/scroll-button.js"></script>
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+ <link rel="stylesheet" href="./styles/scroll-button.css">
 
 <style type="text/css">
 .mobileShow { display: none;}
