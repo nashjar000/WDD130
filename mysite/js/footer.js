b8efd726ff7ac928ef7d8ff4165e08d21ef8e9c5
Jared Nash <nash_jar000@byui.edu>
2023-09-22 15:51:33 -0700

    added a link to my git repo

--- mysite/js/footer.js
+++ mysite/js/footer.js
@@ -5,28 +5,41 @@ footer.innerHTML = `
 <div class="social">
     <button id="shareButton" class="share-button">Share This Page</button>
     <script>
- // Add a click event listener to the share button
- document.getElementById("shareButton").addEventListener("click", sharePage);
-
- // Function to share the current page using the Web Share API
- function sharePage() {
-     if (navigator.share) {
-         navigator.share({
-             title: "Your Website",
-             text: "Check out this website!",
-             url: window.location.href
-         })
-         .then(() => {
-             console.log("Shared successfully");
-         })
-         .catch(error => {
-             console.error("Error sharing:", error);
-         });
-     } else {
-         console.log("Web Share API not supported on this device.");
-         // You can provide a fallback sharing option here if the Web Share API is not supported.
-     }
- }
+    // Add a click event listener to the share button
+    document.getElementById("shareButton").addEventListener("click", sharePage);
+    
+    // Function to share the current page using the Web Share API or copy the link to the clipboard
+    function sharePage() {
+        // Check if the Web Share API is supported
+        if (navigator.share) {
+            navigator.share({
+                title: "Your Website",
+                text: "Check out this website!",
+                url: window.location.href
+            })
+            .then(() => {
+                console.log("Shared successfully");
+            })
+            .catch(error => {
+                console.error("Error sharing:", error);
+            });
+        } else {
+            console.log("Web Share API not supported on this device.");
+    
+            // Fallback for copying the link to the clipboard
+            const url = window.location.href;
+            const textArea = document.createElement("textarea");
+            textArea.value = url;
+            document.body.appendChild(textArea);
+            textArea.select();
+            document.execCommand("copy");
+            document.body.removeChild(textArea);
+    
+            console.log("Link copied to clipboard:", url);
+            alert("Link copied to clipboard!");
+        }
+    }
+    
 </script>
     <a href="https://www.facebook.com/jared.nash.108/" target="_blank">
         <img src="images/facebook.png" alt="fb icon">
